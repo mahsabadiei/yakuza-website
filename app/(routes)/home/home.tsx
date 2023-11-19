a4f7e19877ecc1560ae88b423c4a907371b23b79
Mahsa Badiei <mahsabadiei@Mahsas-MacBook-Pro.local>
2023-11-19 18:40:44 +0330

    Add mobile responsive ui

--- app/(routes)/home/home.tsx
+++ app/(routes)/home/home.tsx
@@ -19,17 +19,17 @@ export default function Home() {
   return (
     <section className="flex min-h-screen flex-col items-center px-8">
       <span className={baiJamjuree.className}>
-        <p className="text-4xl sm:text-[54px] font-semibold text-center my-[60px]">
+        <p className="text-[32px] sm:text-[54px] font-semibold text-center my-[40px] sm:my-[60px]">
           ヤクザの遺産
         </p>
       </span>
 
-      <div className="flex flex-col md:flex-row min-w-[40vw] gap-4 justify-between rounded-[48px] border border-black dark:border-gray-gradient-light mb-4 px-6 py-4">
+      <div className="flex flex-row w-[358px] sm:w-[587px] gap-4 justify-between rounded-[48px] border border-black dark:border-gray-gradient-light mb-4 px-6 py-4">
         {!session && (
           <>
-            <div className="flex flex-col sm:flex-row items-center justify-center">
+            <div className="flex flex-row items-center justify-center">
               <Image src={xIcon} alt="x logo" priority />
-              <p className="text-base font-semibold leading-6 px-1">
+              <p className="text-xs sm:text-base font-semibold leading-6 px-1">
                 Connect X
               </p>
             </div>
@@ -38,7 +38,7 @@ export default function Home() {
               onClick={() => signIn()}
               className="px-7 py-2 rounded-[32px] bg-black dark:bg-white"
             >
-              <p className="text-sm font-bold leading-[22px] text-white dark:text-black">
+              <p className="text-xs sm:text-sm font-bold leading-[22px] text-white dark:text-black">
                 Connect
               </p>
             </button>
@@ -47,9 +47,9 @@ export default function Home() {
 
         {session && (
           <>
-            <div className="flex flex-col sm:flex-row items-center justify-center">
+            <div className="flex flex-row items-center justify-center">
               <Image src={xIcon} alt="x logo" priority />
-              <p className="text-base font-semibold leading-6 px-1">
+              <p className="text-xs sm:text-base font-semibold leading-6 px-1">
                 {session.user?.name}
               </p>
             </div>
@@ -58,7 +58,7 @@ export default function Home() {
               onClick={() => signOut()}
               className="px-7 py-2 rounded-[32px] bg-black dark:bg-white"
             >
-              <p className="text-sm font-bold leading-[22px] text-white dark:text-black">
+              <p className="text-xs sm:text-sm font-bold leading-[22px] text-white dark:text-black">
                 Disconnect
               </p>
             </button>
@@ -67,39 +67,39 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col sm:flex-row w-full gap-10 items-center justify-between ">
-        <div className="flex flex-col items-center justify-center px-2 py-7 rounded-[32px] bg-black">
-          <Link href={""} target="_blank" rel="noopener noreferrer">
+        <div className="flex flex-row sm:flex-col gap-6 items-center justify-center px-2 py-[10px] sm:py-7 rounded-[32px] bg-black">
+          <Link
+            href={""}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <Image src={xIcon} alt="xIcon" />
           </Link>
           <Link
             href={""}
             target="_blank"
             rel="noopener noreferrer"
-            className="mt-6"
           >
             <Image src={youtubeIcon} alt="youtubeIcon" />
-          </Link>{" "}
+          </Link>
           <Link
             href={""}
             target="_blank"
             rel="noopener noreferrer"
-            className="mt-6"
           >
             <Image src={coinmarketcapIcon} alt="coinmarketcapIcon" />
-          </Link>{" "}
+          </Link>
           <Link
             href={""}
             target="_blank"
             rel="noopener noreferrer"
-            className="mt-6"
           >
             <Image src={coingeckoIcon} alt="coingeckoIcon" />
-          </Link>{" "}
+          </Link>
           <Link
             href={""}
             target="_blank"
             rel="noopener noreferrer"
-            className="mt-6"
           >
             <Image src={discordIcon} alt="discordIcon" />
           </Link>
@@ -107,7 +107,6 @@ export default function Home() {
             href={""}
             target="_blank"
             rel="noopener noreferrer"
-            className="mt-6"
           >
             <Image src={gitbookIcon} alt="gitbookIcon" />
           </Link>
@@ -115,13 +114,12 @@ export default function Home() {
             href={""}
             target="_blank"
             rel="noopener noreferrer"
-            className="mt-6"
           >
             <Image src={dextoolsIcon} alt="dextoolsIcon" />
           </Link>
         </div>
 
-        <div className="w-[585px] h-[656px] relative">
+        <div className="w-[352px] h-[428px] sm:w-[585px] sm:h-[656px] relative">
           <Image
             src={human}
             alt="human"
@@ -151,7 +149,9 @@ export default function Home() {
           />
         </div>
 
-        <div>
+        <div />
+
+        <div className="absolute right-0 bottom-[250px]">
           <span className={sunrise.className}>
             <p className="text-xl font-normal rotate-90">Whitepaper</p>
           </span>
