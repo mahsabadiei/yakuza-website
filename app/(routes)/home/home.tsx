3e8970500ffc5dadfd280061dbc55cc609aebafc
Mahsa Badiei <mahsabadiei@Mahsas-MacBook-Pro.local>
2023-12-02 18:53:25 +0330

    Test react-player

--- app/(routes)/home/home.tsx
+++ app/(routes)/home/home.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useRef } from "react";
+import ReactPlayer from "react-player";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { motion } from "framer-motion";
 import { FramerMotionWrapper } from "@/app/shared/components/framer-motion-wrapper/framer-motion-wrapper";
@@ -176,7 +177,17 @@ export default function Home() {
           </div>
         </div>
 
-        <video
+        <ReactPlayer
+          className="object-cover min-w-[100%] min-h-[628px] sm:min-h-[100%] fixed right-0 bottom-0 -z-10"
+          url="./human-head.mp4"
+          playsInline
+          loop
+          muted
+          // ref={videoEl}
+          playing
+        />
+
+        {/* <video
           className="object-cover min-w-[100%] min-h-[628px] sm:min-h-[100%] fixed right-0 bottom-0 -z-10"
           // autoPlay
           playsInline
@@ -186,7 +197,7 @@ export default function Home() {
         >
           <source src="./human-head.mp4" type="video/mp4" />
           Your browser does not support the video tag.
-        </video>
+        </video> */}
       </motion.section>
     </FramerMotionWrapper>
   );
