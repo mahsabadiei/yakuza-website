d6eba5f0e7f741855cf2e57e924e2eaaa8d8808f
Mahsa Badiei <mahsabadiei@Mahsas-MacBook-Pro.local>
2023-12-02 18:31:57 +0330

    Remove light mode from css

--- app/(routes)/home/home.tsx
+++ app/(routes)/home/home.tsx
@@ -177,7 +177,7 @@ export default function Home() {
         </div>
 
         <video
-          className="object-cover min-w-[100%] min-h-[100%] fixed right-0 bottom-0 -z-10"
+          className="object-cover min-w-[100%] min-h-[628px] sm:min-h-[100%] fixed right-0 bottom-0 -z-10"
           // autoPlay
           playsInline
           loop
